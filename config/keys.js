19076e9f6e1b27d64a71e662e2f0aba46543dc0d
Trent Green <70169861+Trent325@users.noreply.github.com>
2021-12-01 16:04:50 -0500

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+dbPassword = 'mongodb+srv://Trent:'+ encodeURIComponent('Pobloy107') + 'Cluster0.mongodb.net/test?retryWrites=true';
+module.exports = ({
+    MongoURI:'mongodb+srv://Trent:Pobloy107@cluster0.eiamr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+})
