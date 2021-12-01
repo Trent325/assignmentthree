b17bd83c16bfe31ec67ac79407190c4fbad26cbf
Trent Green <70169861+Trent325@users.noreply.github.com>
2021-12-01 16:05:56 -0500

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
+
+//about us page
+
+router.get('/aboutUs', (req,res)=>{
+    res.render('aboutUs')
+})
+
+module.exports = router;
